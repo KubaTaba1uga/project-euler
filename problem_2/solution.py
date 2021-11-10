eaef9c8d075c38157722c1a40725bec9bb34eba2
KubaTaba1uga <jakub@buczynski.net.pl>
2021-11-10 22:09:14 +0100

    fix names

--- problem_2/solution.py
+++ problem_2/solution.py
@@ -1,13 +1,13 @@
-def generate_fibbonacci_numbers(stop_value, value_0=0, value_1=1, sum=0):
+def find_even_number_fb(stop_value, value_0=0, value_1=1, sum=0):
     if value_0 < stop_value:
         result = value_0 + value_1
         if result % 2 == 0:
             sum += result
-        return generate_fibbonacci_numbers(stop_value, value_1, result, sum)
+        return find_even_number_fb(stop_value, value_1, result, sum)
     else:
         return sum
 
 
-result = generate_fibbonacci_numbers(4000000)
+result = find_even_number_fb(4000000)
 
-print('sum', result)
+print('result', result)
