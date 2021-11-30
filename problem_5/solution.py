3776bf9812bc0a4b9548e37753ea36e16d5889c7
KubaTaba1uga <jakub@buczynski.net.pl>
2021-11-30 19:59:36 +0100

    add problem 5 solution

--- problem_5/solution.py
+++ problem_5/solution.py
@@ -2,7 +2,7 @@ number = 0
 while True:
     number += 2
     checksum = [True for i in range(1, 20) if number % i == 0]
-    print(number)
     if len(checksum) == 19:
-        print('BANG', number)
         break
+
+print(number)
